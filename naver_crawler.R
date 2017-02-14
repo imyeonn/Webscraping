73b63cfaeeef5225e88612de29d9ad31d9d5c0e4
Hyeyeon Yang <imyeonn@gmail.com>
2017-02-14 19:34:25 +0900

    네이버 실시간검색어 크롤러

--- naver_crawler.R
+++ naver_crawler.R
@@ -3,7 +3,7 @@ Encoding(text) = "UTF-8"
 
 text_sub = text[589:608]
 
-for(n in 1: length(text_sub)){
+for(n in 1:length(text_sub)){
   start_p = gregexpr(text_sub[n], pattern = ">")[[1]][1] + 1 # 시작위치
   end_p = gregexpr(text_sub[n], pattern = "<")[[1]][2] - 1 #종료위치
   text_sub[n] = substr(text_sub[n], start_p, end_p)
