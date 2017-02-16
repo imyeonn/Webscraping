877fbb8809a1f664e73e52b5b741a4327738b805
Hyeyeon Yang <imyeonn@gmail.com>
2017-02-17 00:37:31 +0900

    chp7

--- practice0219.py
+++ practice0219.py
@@ -5,4 +5,18 @@ for i in range(1, 6) :
 
         print()
 
-# 
+# 가변 매개변수(*: 튜플)
+def merge_string(*text_list):
+    result = ' '
+    for s in text_list:
+        result = result + s
+    return result
+
+merge_string('아버지가', '방에', '들어가신다.')
+
+# 가변 매개변수(**: 딕셔너리)
+def print_team(**players):
+    for k in players.keys():
+        print('{0} = {1}'.format(k, players[k]))
+
+print_team(카시야스='GK', 호날두='FW', 알론소='MF', 패패='DF')
