636240643a93f6d9f690fbcbed737eefa149050e
Hyeyeon Yang <imyeonn@gmail.com>
2017-02-17 08:20:48 +0900

    chp7

--- practice0219.py
+++ practice0219.py
@@ -20,3 +20,17 @@ def print_team(**players):
         print('{0} = {1}'.format(k, players[k]))
 
 print_team(카시야스='GK', 호날두='FW', 알론소='MF', 패패='DF')
+
+# 일반매개변수+키워드매개변수
+def print_args(argc, *argv):
+    for i in range(argc):
+        print(argv[i])
+
+print_args(3, 'arg1', 'arg2', 'arg3')
+
+# 키워드매개변수+일반매개변수
+def print_args(*argc, argv):
+    for i in range(argc):
+        print(argv[i])
+
+print_args('arg1', 'arg2', 'arg3', argc=3)
